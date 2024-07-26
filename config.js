3b1cf0fe9ed6d88786779c96503bb67dd7a7b0cf
hostinekeyo <hostinekeyo@gmail.com>
2024-07-26 16:57:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const fs = require("fs-extra");
 if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
-global.audio = "";
-global.video = "";
+global.audio = "oui";
+global.video = "oui";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
 global.email = "TalkDrove@gmail.com";
@@ -21,8 +21,8 @@ global.devs = "923072380380";
 global.sudo = process.env.SUDO || "923072380380";
 global.owner = process.env.OWNER_NUMBER || "923072380380";
 global.style = process.env.STYLE || "3";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
+global.gdbye = process.env.GOODBYE || "all";
+global.wlcm = process.env.WELCOME || "all";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
@@ -33,7 +33,7 @@ global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
 global.read_status = process.env.AUTO_READ_STATUS || "false";
-global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "all";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "all";
 global.read_status_from = process.env.READ_STATUS_FROM || "all";
 
@@ -44,7 +44,7 @@ global.scan = "https://byte-session.vercel.app/";
 
 
 global.SESSION_ID =
-  process.env.SESSION_ID ||
+  process.env.SESSION_ID |Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR0JLNzZUNmNQMXJtUkRYYndscElsSWttNUVhMkpZbzB4eDhHYmowdFdrYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWThtYXBLNXBDYXpMbFRwcUlVekRhOVArR3p2cWEyTS9XRzR5ZmY4WCtFdz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJvTFd0RXdpUXJVcENLeFhCcFdVY0hlYW15QkNlOHJENUV6akxQdHZzSGxFPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjNW5QRHUxUU5UclJKQ0xzODVCZVFTelk2d0d5emFJUmx5VVhyVmYycEdnPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im9GRkFYdDRDZGlxb2FWc3l3QldzY1BCRXhWbnFIajZzakhKWThGRVp2V1k9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVVZWhJTGtxbzVTb2VPUnl1ZlZBQlZqbVhaYW83cVVhcFoxWEYwM3h3dzQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSU9PNVJyMDNXTTJJSGwzWVg0MUdCOFFhSGVCVmtQRlZDbUFvUVFpWWQyRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNVpxUDRRV1R1WUVpRWUwMmtVWFVXYm5ZWHBkcFRxSkptUnYyOFlGbFJERT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkhRNVByVWFFV1BBL3ZsUi9heUFjVkVqejg4NUJHeS82N1lJbzhoQWNRc1VhZ3VncDNuSlBVcGpHeWpyeWdhdTdQZTB1dForKzQvVFdUa0VRZmFLSkNnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6ODIsImFkdlNlY3JldEtleSI6IlRnSnZvK1YrdFo0NjgwblZyT2FxUUdLZE1Ncmg0YmZvaTR3cnZBMmJrTm89IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImN0Mm9DQVA4UnFXMnpqS3ppdVJaLVEiLCJwaG9uZUlkIjoiZTljOTBjMTgtYmUyYi00MGYxLWJjM2ItNTg5OTNlYzU0YWU2IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik1HOTJSWW53OUFGcnFNZ1MydThzeUNwaVhLbz0ifSwicmVnaXN0ZXJlZCI6ZmFsc2UsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid3RhbkFjZlozbk5BK0NzWVFUUHZydkdnWng0PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSW5IN2U4RkVMamhqclVHR0JFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiT1FwWlJpNGlZSWJrRWplaFppb1I2NEdjNHZTazdxcFhqNnYrQ1NoTUJnST0iLCJhY2NvdW50U2lnbmF0dXJlIjoiaFVEZ3gvRUc0QmNCSGtnWFVGa0NZRG1IVmttbXVvRVNyc1czVnZSOEhiWFVPeHpjRFloTE4rRHRKbjRXbUw1bi9lemlDdmJJbnhKeGt3SnpidXIrQnc9PSIsImRldmljZVNpZ25hdHVyZSI6IjNHWFpsd3pBcWRGM21WWlljODdpNWlkUXdwcHY5aVNtakZzcWZER3FJS2lFSmpwQi9ieXlNTzdWT3k1RU9hOFdXUmNiS0pZZVZFdWtjUU04ZUQyZEJnPT0ifSwibWUiOnsiaWQiOiIyMjU2Njc1NzEzMjoyNEBzLndoYXRzYXBwLm5ldCJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMjU2Njc1NzEzMjoyNEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJUa0tXVVl1SW1DRzVCSTNvV1lxRWV1Qm5PTDBwTzZxVjQrci9na29UQVlDIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIyMDAzNjU3fQ==|
 
 
 
